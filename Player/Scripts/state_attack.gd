faa629055063e92fa19160a13ac7a0f2827dee96
unknown <ajmalmahir@icloud.com>
2025-02-28 00:00:00 +0000

    finished making the attack animations just linking them up now

---
+++ Player/Scripts/state_attack.gd
@@ -0,0 +1,27 @@
+class_name State_Attack extends State
+
+@onready var walk: State_Walk = $"../Walk"
+
+## What happens when the player enters this state?
+func Enter() -> void:
+	player.UpdateAnimation("attack")
+	pass
+
+
+## what happens when the player exits this state?
+func Exit() -> void:
+	pass
+
+
+## what happens during the _process update in this state?
+func Process( _delta : float ) -> State:
+	player.velocity = Vector2.ZERO
+	return null
+
+## what happens during the _physics_process update in this state?
+func Physics( _delta : float ) -> State:
+	return null
+
+## what happens with input events in this state?
+func HandleInput( _event: InputEvent ) -> State:
+	return null
